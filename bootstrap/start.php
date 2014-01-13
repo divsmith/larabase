1f321b8b4920697a6d040bddd0e719a19c635432
Parker Smith <parker@parkersmith.us>
2014-01-13 04:28:45 +0000

    Add development environment configuration

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('your-machine-name'),
-
-));
+//putenv('ENV=production');
+$env = $app->detectEnvironment(function()
+{
+	return getenv('ENV') ?: 'development';
+});
 
 /*
 |--------------------------------------------------------------------------
