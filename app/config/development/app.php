c4769f0f29c58c4e87d5bc3ac4b7e4f81f9e19a5
Parker Smith <parker@parkersmith.us>
2014-03-05 16:09:26 -0700

    Add generators

---
+++ app/config/development/app.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+	'providers' => array(
+
+			'Illuminate\Foundation\Providers\ArtisanServiceProvider',
+			'Illuminate\Auth\AuthServiceProvider',
+			'Illuminate\Cache\CacheServiceProvider',
+			'Illuminate\Session\CommandsServiceProvider',
+			'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
+			'Illuminate\Routing\ControllerServiceProvider',
+			'Illuminate\Cookie\CookieServiceProvider',
+			'Illuminate\Database\DatabaseServiceProvider',
+			'Illuminate\Encryption\EncryptionServiceProvider',
+			'Illuminate\Filesystem\FilesystemServiceProvider',
+			'Illuminate\Hashing\HashServiceProvider',
+			'Illuminate\Html\HtmlServiceProvider',
+			'Illuminate\Log\LogServiceProvider',
+			'Illuminate\Mail\MailServiceProvider',
+			'Illuminate\Database\MigrationServiceProvider',
+			'Illuminate\Pagination\PaginationServiceProvider',
+			'Illuminate\Queue\QueueServiceProvider',
+			'Illuminate\Redis\RedisServiceProvider',
+			'Illuminate\Remote\RemoteServiceProvider',
+			'Illuminate\Auth\Reminders\ReminderServiceProvider',
+			'Illuminate\Database\SeedServiceProvider',
+			'Illuminate\Session\SessionServiceProvider',
+			'Illuminate\Translation\TranslationServiceProvider',
+			'Illuminate\Validation\ValidationServiceProvider',
+			'Illuminate\View\ViewServiceProvider',
+			'Illuminate\Workbench\WorkbenchServiceProvider',
+			'Way\Generators\GeneratorsServiceProvider'
+		)
+);
