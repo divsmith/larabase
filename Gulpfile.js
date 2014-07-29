eaadcd41720eb5def07c8f2f902a85acf53eebde
Parker Smith <parker@parkersmith.us>
2014-07-29 17:43:11 -0600

    Add js and css tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,30 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	livereload = require('gulp-livereload'),
-	minify_css = require('gulp-minify-css'),
+	minifyCSS = require('gulp-minify-css'),
 	uglify = require('gulp-uglify');
 
+var paths = {
+	js: [
+		'app/assets/components/jquery/dist/jquery.js',
+		'app/assets/components/bootstrap/dist/js/bootstrap.min.js'
+	],
 
+	css: [
+		'app/assets/components/bootstrap/dist/css/bootstrap.min.css'
+	]
+}
+
+gulp.task('js', function() {
+	gulp.src(paths.js)
+		.pipe(concat('build.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('css', function() {
+	gulp.src(paths.css)
+		.pipe(concat('build.css'))
+		.pipe(minifyCSS({keepSpecialComments: 0}))
+		.pipe(gulp.dest('./public/css'));
+})
