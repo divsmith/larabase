f58c5d9ab850ab32aa27f25d370665e7353318dd
Parker Smith <parker@parkersmith.us>
2014-07-28 22:58:43 -0600

    Add gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	livereload = require('gulp-livereload'),
+	minify_css = require('gulp-minify-css'),
+	uglify = require('gulp-uglify');
+
+
